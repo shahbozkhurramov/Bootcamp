2d2a119a6c6b353e735367f2140dcbcbe1e2a33c
shahbozkhurramov <shahbozkhurramov@gmail.com>
2021-10-06 20:59:21 +0500

    lab5

--- homework/Solutions/lab5.cs
+++ homework/Solutions/lab5.cs
@@ -88,5 +88,95 @@ namespace homework.Solutions
             else if(n==2) Problem9();
             else Problem10();
         }
+        public void Problem13(){
+            var ints = Console.ReadLine().Split().Select(int.Parse).ToList();
+            if(ints[0]+ints[1]>100) Console.WriteLine("true");
+            else Console.WriteLine("false");
+        }
+        public void Problem14(){
+            var ints = Console.ReadLine().Split().Select(int.Parse).ToList();
+            ints.Sort();
+            if(Math.Pow(ints[0], 2)+Math.Pow(ints[1], 2)==Math.Pow(ints[2], 2)) Console.WriteLine("true");
+            else Console.WriteLine("false");
+        }
+        public void Problem15(){
+            var c=Console.ReadLine();
+            if(c=="-" || c=="+" || c=="*" || c=="/" || c=="%") Console.WriteLine("true");
+            else Console.WriteLine("false");
+        }
+        public void Problem16(){
+            int son=int.Parse(Console.ReadLine());
+            var mes=son switch 
+            {
+                1=>$"bir",
+                0=>$"nol",
+                2=>$"ikki",
+                3=>$"uch",
+                4=>$"to'rt",
+                5=>$"besh",
+                6=>$"olti",
+                7=>$"yetti",
+                8=>$"sakkiz",
+                9=>$"to'qqiz",
+                _=>$"bpshqa"
+            };
+            Console.WriteLine(mes);
+        }
+        public void Problem17(){
+            var ints = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int a=ints[0],b=ints[1],c;
+            if(a>=1)
+            {
+                if(b>=45)
+                {
+                c=b-45;
+                a=a;
+                }
+                else
+                {
+                c=b+15;
+                a=a-1;
+                }
+            }
+            else{
+                if(b>=45)
+                {
+                c=b-45;
+                a=a;
+                }
+                else
+                {
+                c=b+15;
+                a=23;
+                }  
+            }
+            Console.WriteLine($"{a} {c}");
+        }
+        public void Problem18(){
+            int n=int.Parse(Console.ReadLine());
+            if(n>=90) Console.WriteLine("A");
+            else if(n>=80) Console.WriteLine("B");
+            else if(n>=70) Console.WriteLine("C");
+            else if(n>=60) Console.WriteLine("D");
+            else Console.WriteLine("F");
+        }
+        public void Problem19(){
+            int a=int.Parse(Console.ReadLine());
+            if(a>=3000 && a<=1000000){
+                if(a % 1000 >= 500) Console.WriteLine((a/1000)*1000+1000);
+                else Console.WriteLine((a/1000)*1000);
+            }
+        }
+        public void Problem20(){
+            var a=Console.ReadLine();
+            if((a[0]>='A' && a[0]<='Z') || (a[0]>='a' && a[0]<='z')) Console.WriteLine("1");
+            else Console.WriteLine("0");
+        }
+        public void Problem21(){
+            int a=int.Parse(Console.ReadLine());
+            if(a%2==0) Console.WriteLine("even");
+            else Console.WriteLine("odd");
+        }
+
     }
 }
